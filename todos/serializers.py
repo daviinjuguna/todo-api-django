af5791201bd08baf2330c72f1c6594c50414f79f
daviinjuguna <njugunadee25@gmail.com>
2021-08-27 06:56:00 +0300

    edited

--- todos/serializers.py
+++ todos/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import Todo
 
+
 class  TodoSerializer(serializers.ModelSerializer):
+    title = serializers.CharField()
+    description = serializers.CharField()
     class Meta:
         model=Todo
         fields ='__all__'
